4ea456b5dbbd65b5aa96a05168829c3f5bf4d708
이종경 <gyoung3063413@naver.com>
2023-08-01 20:55:57 +0900

    [추가] 로그인 기능 추가 및 리팩토링

--- server/application/views.py
+++ server/application/views.py
@@ -2,6 +2,7 @@ from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.contrib.auth import authenticate, login
+from django.contrib.sessions.models import Session
 from django.http import Http404
 
 from .serializers import ReviewSerializer
@@ -49,14 +50,21 @@ class ReviewDetail(APIView):
 
 class LoginView(APIView):
     def post(self, request):
+        # FE로부터 받은 아이디,비밀번호
         username = request.data.get('username')
         password = request.data.get('password')
 
+        # DB와 비교
         user = authenticate(username=username, password=password)
 
-        if user:
+        if user is not None: # 계정이 있을경우
             login(request, user)
-            return Response({'message': 'Login Successful'})
+
+            user_data = {
+                'id':user.id,
+                'username':user.username
+            }
+            return Response(user_data)
         else:
             return Response({'error': 'Invalud credentials'}, status=400)
 
