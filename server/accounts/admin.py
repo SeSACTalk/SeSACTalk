7faefdd750e8d5d3c9ef65d72b116507ed05799c
이종경 <gyoung3063413@naver.com>
2023-08-09 22:04:53 +0900

    [추가] User 모델 커스터마이징

--- server/accounts/admin.py
+++ server/accounts/admin.py
@@ -1,1 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
+
+# UserAdmin을 상속하여 User 모델을 관리하는 Admin 클래스 정의
+class CustomUserAdmin(UserAdmin):
+    model = User
+    list_display = ['username', 'email', 'name', 'is_active', 'is_staff', 'is_superuser']
+
+# UserAdmin 클래스를 등록하여 User 모델을 Admin 패널에 표시
+admin.site.register(User, CustomUserAdmin)
--- server/accounts/models.py
+++ server/accounts/models.py
@@ -1,13 +1,64 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
-# class User(AbstractUser):
-#     name = models.CharField(max_length=20),
-#     # brithdate = models.DateField(),
-#     # gender = models.CharField(max_length=10),
-#     # phonenumber = models.CharField(max_length=15),
-#     # course_id1 = models.ForeignKey()
-#     # course_id2 = models.ForeignKey(null=True)
+# 캠퍼스, 과정, 사용자
+class Campus(models.Model):
+    name = models.CharField(max_length = 10)
+    address = models.CharField(max_length = 100)
 
-#     first_name = None,
-#     last_name = None,
+class Course(models.Model):
+    name = models.CharField(max_length = 100)
+    campus_id = models.ForeignKey(Campus, on_delete = models.CASCADE)
+
+class UserManager(BaseUserManager):
+    use_in_migrations = True    
+
+    def _create_user(self, username, email, password, **extra_fields):
+        if not username:
+            raise ValueError('Username을 입력해주세요.')
+        if not email:
+            raise ValueError('Email을 입력해주세요.')
+        email = self.normalize_email(email)
+        user = self.model(username=username, email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, username, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('is_staff=True일 필요가 있습니다.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('is_superuser=True일 필요가 있습니다.')
+        
+        return self._create_user(username, email, password, **extra_fields)
+    
+class User(AbstractBaseUser, PermissionsMixin):
+    id = models.BigAutoField(primary_key = True)
+    username = models.CharField(max_length = 20, unique = True)
+    name = models.CharField(max_length = 20),
+    biㄱthdate = models.DateField(),
+    gender = models.CharField(max_length = 10),
+    phone_number = models.CharField(max_length = 15),
+    email = models.EmailField(unique = True)
+    signup_date = models.DateTimeField(auto_now_add = True)
+    withdraw_date = models.DateTimeField(null = True)
+    first_course_id = models.ForeignKey(Course, on_delete = models.CASCADE, related_name = 'first_course')
+    second_course_id = models.ForeignKey(Course, on_delete = models.CASCADE, null = True, related_name = 'second_course')
+
+    objects = UserManager()
+
+    is_auth = models.IntegerField(default = 0)
+    #! 0: 가입, 1: 승인, 2: 보류, 3: 거절
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email']
+
+    def __str__(self):
+        return self.username
