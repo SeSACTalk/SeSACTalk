a8a716d94c417ec7e1fc8343224d3012a17bb7ae
이종경 <gyoung3063413@naver.com>
2023-08-14 11:00:22 +0900

    [추가] URL 매핑

--- server/accounts/urls.py
+++ server/accounts/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
-from accounts.views import LoginView, SignUpView
+from accounts.views import LoginView, CheckIdView
 
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
-    path('signup/',SignUpView.as_view(), name='signUp')
+    path('check/id/',CheckIdView.as_view(), name='checkId')
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
