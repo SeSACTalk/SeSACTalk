c6f6f030ce3c3925d029e2f67c48a75edc084a6d
KIM SOOBIN <xpsxm225@naver.com>
2023-08-20 20:43:59 +0900

    [수정] - 최종 response를 각 method의 역할에 맞게 수정 - 설명 주석 추가 - 코드를 가독성 있게 리팩토링

--- server/post/views.py
+++ server/post/views.py
@@ -11,117 +11,122 @@ from post.mixins import OwnerPermissionMixin
 from post.constants import ResponseMessages
 from user.models import UserRealtionship
 
+""" >> 전달사항
+    Clas Post View
+    - 전체 포스트 리스트 조회, 포스트 업로드 views
+    
+    Class PostDetail View
+    - 포스트 디테일 보기(read), 포스트 수정하기(update), 포스트 삭제하기(delete) views
+    
+    OwnerPermissionMixin: post에 접근하는 주체가 로그인 주체와 같은지, 권한 검사하는 mixin 클래스.
+    - check_post_owner(frontend_session_key: str, username: str, option: str = None)-> Union[bool, tuple]
+        * frontend_session_key: front에서 보내는 session_key
+        * username: 경로변수로 접근한 username값
+        * option: 
+            ** None: 기본값으로, return은 bool값. True == 로그인주체와 접근 주체 같음 / False == 로그인 주체와 접근 주체 다름
+            ** 'get_owner'를 인자로 전달하면 return값이 튜플, user_post_owner(로그인한 User Model), condition(위의 bool값)
+            
+    반환 메시지를 상수를 정의하는 모듈에 정의
+    - post.constants.py의 ResponseMessages클래스(클래스 변수)
+            
+    select_related()함수는 N+1쿼리문제를 해결하기 위해 사용(cf. prefetch_related())
+    - Forien key로 연결된 객체 데이터를 미리 가져오는 역할.
+"""
 class Post(APIView, OwnerPermissionMixin):
-    """
-        전체 포스트 리스트 조회, 포스트 업로드 views
-    """
     def get(self, request: HttpRequest, username) -> Response:
+        # 권한 확인
         user_who_accessed_post, condition_read_user_is_same_as_login_user = self.check_post_owner\
                                                                             (request.META.get('HTTP_AUTHORIZATION'),\
                                                                              username,\
                                                                              'get_owner')
-
         if not condition_read_user_is_same_as_login_user:
             return Response({'error': ResponseMessages.FORBIDDEN_ACCESS}, status.HTTP_403_FORBIDDEN)
 
-        """ select_related()함수는 N+1쿼리문제를 해결하기 위해 사용(cf. prefetch_related())
-        Forien key로 연결된 객체 데이터를 미리 가져오는 역할. """
+        # 팔로우 기반 또는 자신의 게시물 포스트를 가져오는 쿼리문 수행, order by의 - 기호는 역순을 의미
         user_s_follows = UserRealtionship.objects.filter(user_follower=user_who_accessed_post.id)
-
-        # 팔로우한 사용자들의 포스트를 가져오는 쿼리 / - 기호는 역순을 의미
-        posts_by_followed_user = PostModel.objects.filter(
+        posts_by_followed_user_or_own = PostModel.objects.filter(
             Q(user=user_who_accessed_post.id) | Q(user__in=user_s_follows.values('user_follow'))
         ).select_related('user').order_by('-date')
 
-        if bool(posts_by_followed_user):
-            postSerializer = PostSerializer(posts_by_followed_user, many=True)
+        # QuerySet이 비어있을 경우
+        if not bool(posts_by_followed_user_or_own):
+            return Response({'message': ResponseMessages.NO_POSTS_TO_DISPLAY}, status=status.HTTP_200_OK)
+        
+        # 반환할 게시물이 있는 경우
+        postSerializer = PostSerializer(posts_by_followed_user_or_own, many=True)
+        for i, post in enumerate(posts_by_followed_user_or_own): postSerializer.data[i]['username'] = post.user.username
 
-            for i, post in enumerate(posts_by_followed_user):
-                user = post.user
-                postSerializer.data[i]['username'] = user.username
+        return Response(postSerializer.data, status=status.HTTP_200_OK)
 
-            return Response(postSerializer.data, status=status.HTTP_200_OK)
 
-        return Response({'message' : ResponseMessages.NO_POSTS_TO_DISPLAY}, status=status.HTTP_200_OK)
 
+    # TODO: 파일 유형 검증, 파일 크기 제한, 보안적 검증 등에 대한 오류 처리 및 로깅하기
     def post(self, request: HttpRequest, username) -> Response:
-        # TODO: 파일 유형 검증, 파일 크기 제한, 보안적 검증 등에 대한 오류 처리 및 로깅하기
+        # 권한 확인
+        user_who_accessed_post, condition_posting_user_is_same_as_login_user = self.check_post_owner(\
+                                                                                request.META.get('HTTP_AUTHORIZATION'),\
+                                                                                username,\
+                                                                                'get_owner')
+        if not condition_posting_user_is_same_as_login_user:
+            return Response({'error': ResponseMessages.FORBIDDEN_ACCESS}, status.HTTP_403_FORBIDDEN)
 
-        # get [login user, condition(접근==login user)]
-        user_who_accessed_post, condition_posting_user_is_same_as_login_user = self.check_post_owner\
-            (request.META.get('HTTP_AUTHORIZATION'),\
-             username,\
-             'get_owner')
+        # 이미지 파일이 없을 때, 컨텐츠의 길이가 500자를 넘을 때 예외처리
+        try:
+            content = request.data['content'].strip()
+            if not (content and len(content) <= 500):
+                raise ValueError("Content length less than 0 or exceeded: 500")
+            img_path = request.FILES['img_path']
+            post = PostModel.objects.create(content=content, img_path=img_path, user=user_who_accessed_post)
+        except ValueError as ve:
+            print(ve)
+            return Response({'error': ResponseMessages.CONTENT_LENGTH_EXCEEDED}, status.HTTP_400_BAD_REQUEST)
+        except MultiValueDictKeyError as exception:
+            print(exception, '\nNo image attachments')
+            post = PostModel.objects.create(content=content, user=user_who_accessed_post)
+        post.save()
 
-        # user_id와 username 값 비교하여 작성 주체 파악
-        if condition_posting_user_is_same_as_login_user:
-            content = request.data['content']
+        return Response({'message': ResponseMessages.CREATE_SUCCESS}, status=status.HTTP_201_CREATED)
 
-            try:  # 이미지 파일이 없을 경우 예외 처리
-                img_path = request.FILES['img_path']
-                post = PostModel.objects.create(content=content, img_path=img_path, user=user_who_accessed_post)
-            except MultiValueDictKeyError as exception:
-                print(exception)
-                post = PostModel.objects.create(content=content, user=user_who_accessed_post)
-
-            if content and len(content) <= 500: # content의 길이 제한(500자 이하)
-                post.save()
-                return Response({'message': ResponseMessages.CREATE_SUCCESS}, status=status.HTTP_201_CREATED)
-            else:
-                print('content의 길이가 500자를 초과')
-                return Response({'error' : ResponseMessages.CONTENT_LENGTH_EXCEEDED}, status.HTTP_400_BAD_REQUEST)
-
-        # 요청이 서버에 도달했지만, 사용자의 경로 조작으로 서버가 해당 요청을 거부
-        return Response({'error' : ResponseMessages.FORBIDDEN_ACCESS}, status.HTTP_403_FORBIDDEN)
 
 
 class PostDetail(APIView, OwnerPermissionMixin):
-    """
-        포스트 디테일 보기(read), 포스트 수정하기(update), 포스트 삭제하기(delete) views
-    """
     def get(self, request: HttpRequest, **kwargs) -> Response:
-        # get condition(접근==login user)
+        # 권한 확인(게시물 주인 확인)
         condition_read_user_is_same_as_login_user = self.check_post_owner(request.META.get('HTTP_AUTHORIZATION'),\
                                                         kwargs['username'])
-
-        # 내가 작성한 글인지
-        response_data = {
-            'isPostMine' : False
-        }
-        if condition_read_user_is_same_as_login_user:
-            response_data['isPostMine'] = True
-
+        response_data = {'isPostMine': condition_read_user_is_same_as_login_user}
         return Response(response_data, status.HTTP_200_OK)
 
     def put(self, request, **kwargs) -> Response:
-        # condition 정의
-        condition_same_from_previous_data = request.data['original_content'] == request.data['update_content']
+        # 권한 확인
         user_who_accessed_post, condition_edit_user_is_same_as_login_user  = self.check_post_owner\
                                                                             (request.META.get('HTTP_AUTHORIZATION'),\
                                                                              kwargs['username'],\
                                                                              'get_owner')
-        if condition_edit_user_is_same_as_login_user:
-            if condition_same_from_previous_data:
-                # update 데이터가 전과 다르지 않아서, update하지 않았음을 응답
-                return Response({'message': ResponseMessages.NOT_UPDATE}, status.HTTP_304_NOT_MODIFIED)
+        if not condition_edit_user_is_same_as_login_user:
+            return Response({'error': ResponseMessages.FORBIDDEN_ACCESS}, status.HTTP_403_FORBIDDEN)
 
-            # update
-            post = PostModel.objects.get(id = kwargs['pk'], user=user_who_accessed_post.id)
-            post.content = request.data['update_content']
-            post.save()
-            return Response({'message': 'UPDATE SUCCESS'}, status.HTTP_200_OK)
+        # update 데이터가 전과 다르지 않아서, update하지 않음
+        if (request.data['original_content'].strip() == request.data['update_content'].strip()):
+            return Response({'message': ResponseMessages.NOT_UPDATE}, status.HTTP_304_NOT_MODIFIED)
 
-        return Response({'error': ResponseMessages.FORBIDDEN_ACCESS}, status.HTTP_403_FORBIDDEN)
+        # update
+        post = PostModel.objects.get(id = kwargs['pk'], user=user_who_accessed_post.id)
+        post.content = request.data['update_content']
+        post.save()
+        return Response({'message': 'UPDATE SUCCESS'}, status.HTTP_200_OK)
+
 
     def delete(self, request, **kwargs) -> Response:
+        # 권한 확인
         user_who_accessed_post, condition_delete_user_is_same_as_login_user  = self.check_post_owner\
                                                                             (request.META.get('HTTP_AUTHORIZATION'),\
                                                                              kwargs['username'],\
                                                                              'get_owner')
+        if not condition_delete_user_is_same_as_login_user:
+            return Response({'error': ResponseMessages.FORBIDDEN_ACCESS}, status.HTTP_403_FORBIDDEN)
 
-        if (condition_delete_user_is_same_as_login_user):
-            post = PostModel.objects.get(id = kwargs['pk'], user = user_who_accessed_post.id)
-            post.delete()
-            return Response({'message' : ResponseMessages.DELETE_SUCCESS}, status.HTTP_204_NO_CONTENT)
-
-        return Response({'error' : ResponseMessages.FORBIDDEN_ACCESS}, status.HTTP_403_FORBIDDEN)
+        # delete
+        post = PostModel.objects.get(id = kwargs['pk'], user = user_who_accessed_post.id)
+        post.delete()
+        return Response({'message' : ResponseMessages.DELETE_SUCCESS}, status.HTTP_204_NO_CONTENT)
