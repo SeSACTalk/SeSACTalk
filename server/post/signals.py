b4f9591f1332b6a51940ddb4100a8bbe2109bfc7
KIM SOOBIN <xpsxm225@naver.com>
2023-08-22 23:10:59 +0900

    [추가] Signal을 이용하여 Post의 record가 삭제될 때, Post의 img_path가 참조하는 경로의 이미지도 삭제.

---
+++ server/post/signals.py
@@ -0,0 +1,12 @@
+import os
+
+from django.db.models.signals import post_delete
+from django.dispatch import receiver
+from post.models import Post
+
+@receiver(post_delete, sender=Post)
+def delete_post_image(sender, instance, **kwargs):
+    # Post의 record가 삭제될 때, Post의 img_path가 참조하는 경로의 이미지도 삭제한다.
+    if instance.img_path:
+        if os.path.isfile(instance.img_path.path):
+            os.remove(instance.img_path.path)
