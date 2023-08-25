dc8b865e520c796432d4efaa54139fe3d5346aae
KIM SOOBIN <xpsxm225@naver.com>
2023-08-25 19:52:56 +0900

    [수정] 비밀번호 찾기 후 임시비밀번호로 1회 로그인 시(로그아웃 후) 재 로그인 안 되도록 수정

--- server/post/mixins.py
+++ server/post/mixins.py
@@ -1,28 +1,15 @@
-from django.contrib.sessions.models import Session
 from typing import Union
 
-from accounts.models import User
+from sesactalk.mixins import SessionDecoderMixin
 
-class OwnerPermissionMixin:
+class OwnerPermissionMixin(SessionDecoderMixin):
     """
         POST에 접근 하는 주체를 검사 하는 Mixin 클래스
         R을 제외한 C, U, D의 시도를 차단 하려는 목적에서 생성.
     """
-
-    def get_post_owner_id(self, frontend_session_key: str) -> int:
-        session = Session.objects.get(session_key=frontend_session_key)
-        user_id = session.get_decoded().get('_auth_user_id')
-
-        return user_id
-
-    def get_post_owner(self, frontend_session_key: str)-> User:
-        user = User.objects.get(id = self.get_post_owner_id(frontend_session_key))
-
-        return user
-
     def check_post_owner(self, frontend_session_key: str, username: str, option: str = None)-> Union[bool, tuple]:
         # get owner
-        user_post_owner = self.get_post_owner(frontend_session_key)
+        user_post_owner = self.get_user_by_pk(frontend_session_key)
 
         # check : owner == 경로 username
         condition = (username == user_post_owner.username)
