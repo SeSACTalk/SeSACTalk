0141f27db390c9de261b97063e2489b772ba2cf8
이종경 <gyoung3063413@naver.com>
2023-09-26 15:34:52 +0900

    [추가] 로그인이 안되어 있을시 console.clear()실행

--- client/src/routes/accounts/Accounts.js
+++ client/src/routes/accounts/Accounts.js
@@ -31,7 +31,6 @@ function Accounts() {
             .catch(
                 error => {
                     console.clear()
-                    console.warn(error)
                 }
             )
     }, []);
